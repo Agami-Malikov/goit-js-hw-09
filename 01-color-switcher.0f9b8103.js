82751da9c606f95d3f0ec0d356cf2ae55ee9ef32
Agami-Malikov <81987422+Agami-Malikov@users.noreply.github.com>
2022-07-18 13:43:15 +0000

    Deploying to gh-pages from @ Agami-Malikov/goit-js-hw-09@07219ffb5a46afb850b173f3dcb2aa0fcde68d85 🚀

---
+++ 01-color-switcher.0f9b8103.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=(document.querySelector("button[data-stop]"),document.querySelector("body"));t.addEventListener("click",(()=>{setInterval((()=>{e.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)}));
+//# sourceMappingURL=01-color-switcher.0f9b8103.js.map
---
+++ 01-color-switcher.4adfe650.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),o=(document.querySelector("button[data-stop]"),document.querySelector("body"));t.addEventListener("click",(function(){setInterval((function(){o.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)}))}();
+//# sourceMappingURL=01-color-switcher.4adfe650.js.map
